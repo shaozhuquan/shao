797663fc665debf77956ebac7104ff5a62dcb429
shaozhuquan <shaozqzq@sina.com>
2018-11-19 09:48:40 +0800

    修改初始页面

--- src/test/java/com/shao/JdbcTest.java
+++ src/test/java/com/shao/JdbcTest.java
@@ -12,20 +12,20 @@ import java.sql.Connection;
 public class JdbcTest {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException  {
-            String URL="jdbc:mysql://127.0.0.1:3306/test?characterEncoding=utf8&useUnicode=true&useSSL=false&serverTimezone=UTC";
+            String URL="jdbc:mysql://47.92.253.244:3306/FitnessDiary?characterEncoding=utf8&useUnicode=true&useSSL=false&serverTimezone=UTC";
             String USER="root";
-            String PASSWORD="root";
+            String PASSWORD="yang1206";
             //1.加载驱动程序
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             //2.获得数据库链接
             Connection conn=DriverManager.getConnection(URL, USER, PASSWORD);
             //3.通过数据库的连接操作数据库，实现增删改查（使用Statement类）
             Statement st=conn.createStatement();
-            ResultSet rs=st.executeQuery("select * from user");
+            ResultSet rs=st.executeQuery("select * from plan");
             //4.处理数据库的返回结果(使用ResultSet类)
             while(rs.next()){
-                System.out.println(rs.getString("name")+" "
-                        +rs.getString("age"));
+                System.out.println(rs.getString("title")+" "
+                        +rs.getString("brief"));
             }
 
             //关闭资源
